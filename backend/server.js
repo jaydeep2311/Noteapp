346012e2e3f0814b8f44b30104b4aacaecc689bf
jaydeep2311 <jaydeep.khatri23@gmail.com>
2025-06-26 09:34:24 +0530

    fix:added 404 route

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,15 @@ app.use(express.json());
 app.use('/auth',require('./routes/auth'))
 app.use('/notes', isAuthenticated , require('./routes/notes'))
 
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: 'Something went wrong!' });
+  });
+  
+  app.use('*', (req, res) => {
+    res.status(404).json({ message: 'Route not found' });
+  });
+  
 app.listen(process.env.PORT,()=>{
     console.log(`Server running on ${process.env.PORT}`)
 })
